914cc98543a20728b492af45bbfb32790a9edfa4
Jonas Buggisch <jonas.buggisch@gmail.com>
2022-12-29 14:29:19 +0100

    Svg support implemented

--- public/wp-content/themes/aaron/functions.php
+++ public/wp-content/themes/aaron/functions.php
@@ -5,4 +5,13 @@ function wpdocs_theme_name_scripts() {
 
 }
 add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = "image/svg+xml";
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+add_theme_support('post-thumbnails');
+
 ?>
